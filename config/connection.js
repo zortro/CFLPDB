37987e41ac2df73daa83187eef940c292e51bc2c
ZETA <ben.r.dominguez@gmail.com>
2022-09-10 13:10:46 -0700

    nothing new

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ let userPass = 'admin'
 const connectString = 
     process.env.MONGODB_URI || `mongodb+srv://${userName}:${userPass}@cflptst.cmwqvdh.mongodb.net/?retryWrites=true&w=majority`
 
-
 connect(connectString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
