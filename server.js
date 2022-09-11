ee2b7c83d62456324fb0770c5185988c461342aa
ZETA <ben.r.dominguez@gmail.com>
2022-09-10 22:15:58 -0700

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const db = require('./config/connection')
 const routes = require('./routes')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001
 const app = express()
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
